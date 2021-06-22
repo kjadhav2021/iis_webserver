cac73b53759f63547108258fd5254c2a88b018ab
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-22 17:15:28 +0800

    Update iis_site.pp

--- manifests/iis_site.pp
+++ manifests/iis_site.pp
@@ -14,23 +14,11 @@ class iis_webserver::iis_site {
       path         => 'C:/inetpub/basic/index.zip',
       source       => 'https://gist.github.com/dylanratcliffe/af0e24303d241b888152bd1cd7c9063d/archive/ad273bebc01c6dac176da7a5f3c38c4d9a584521.zip',
       extract      => true,
-      extract_path => 'C:/inetpub/wwwroot',
-      creates      => 'C:/inetpub/basic', #directory inside tgz
+      extract_path => 'C:/inetpub/wwwroot/',
+      creates      => 'C:/inetpub/basic/', #directory inside tgz
       cleanup      => false,
       require      => File['basic'],
     }
-
-    # archive { $filename:
-    #   path          => "/tmp/${filename}",
-    #   source        => "http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M3/bin/apache-tomcat-9.0.0.M3.zip",
-    #   checksum      => 'f2aaf16f5e421b97513c502c03c117fab6569076',
-    #   checksum_type => 'sha1',
-    #   extract       => true,
-    #   extract_path  => '/opt',
-    #   creates       => $install_path,
-    #   cleanup       => 'true',
-    #   require       => File[$install_path],
-    # }
 
     iis_site { 'basic-site':
       ensure          => 'started',
