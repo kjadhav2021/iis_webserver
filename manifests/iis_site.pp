8e72f8ec4e358799cabc8daa0414a008342bf9dd
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-22 15:24:16 +0800

    Update iis_site.pp

--- manifests/iis_site.pp
+++ manifests/iis_site.pp
@@ -5,22 +5,25 @@
 # @example
 #   include iis_webserver::iis_site
 class iis_webserver::iis_site {
+    file { 'basic':
+      ensure => 'directory',
+      path   => 'c:/inetpub/basic',
+    }
+
     archive { 'index.zip':
       source       => 'https://gist.github.com/dylanratcliffe/af0e24303d241b888152bd1cd7c9063d/archive/ad273bebc01c6dac176da7a5f3c38c4d9a584521.zip',
       extract      => true,
       extract_path => 'C:/inetpub/basic/',
       creates      => 'C:/inetpub/basic/index.html', #directory inside tgz
+      # path         => "${extract_path}/${d['title']}",
       cleanup      => true,
       require      => File['basic'],
     }
+
     iis_site { 'basic-site':
-    ensure          => 'started',
-    physicalpath    => 'c:/inetpub/basic',
-    applicationpool => 'minimal_site_app_pool',
-    require         => File['basic'],
-  }
-  file { 'basic':
-    ensure => 'directory',
-    path   => 'c:/inetpub/basic',
-  }
+      ensure          => 'started',
+      physicalpath    => 'c:/inetpub/basic',
+      applicationpool => 'minimal_site_app_pool',
+      require         => File['basic'],
+    }
 }
