9e1175da644845fe1c060803c54a918e942bd76f
kjadhav2021 <kjadhav102015@gmail.com>
2021-06-22 16:45:55 +0800

    updated iis_site

--- manifests/iis_application_pool.pp
+++ manifests/iis_application_pool.pp
@@ -5,7 +5,7 @@
 # @example
 #   include iis_webserver::iis_application_pool
 class iis_webserver::iis_application_pool {
-  iis_application_pool { 'minimal_site_app_pool':
+  iis_application_pool { 'basic_site_app_pool':
     ensure                  => 'present',
     state                   => 'started',
     managed_pipeline_mode   => 'Integrated',
--- manifests/iis_site.pp
+++ manifests/iis_site.pp
@@ -11,20 +11,31 @@ class iis_webserver::iis_site {
     }
 
     archive { 'index.zip':
+      path         => 'C:/inetpub/basic/index.zip',
       source       => 'https://gist.github.com/dylanratcliffe/af0e24303d241b888152bd1cd7c9063d/archive/ad273bebc01c6dac176da7a5f3c38c4d9a584521.zip',
       extract      => true,
-      extract_path => 'C:/inetpub/basic/',
-      creates      => 'C:/inetpub/basic/index.html', #directory inside tgz
-      # path         => 'C:/inetpub/basic/af0e24303d241b888152bd1cd7c9063d/archive/ad273bebc01c6dac176da7a5f3c38c4d9a584521.zip',
-      path         => 'C:/inetpub/basic/index.html',
+      extract_path => 'C:/inetpub/basic',
+      creates      => 'C:/inetpub/basic', #directory inside tgz
       cleanup      => true,
       require      => File['basic'],
     }
 
+    # archive { $filename:
+    #   path          => "/tmp/${filename}",
+    #   source        => "http://www-eu.apache.org/dist/tomcat/tomcat-9/v9.0.0.M3/bin/apache-tomcat-9.0.0.M3.zip",
+    #   checksum      => 'f2aaf16f5e421b97513c502c03c117fab6569076',
+    #   checksum_type => 'sha1',
+    #   extract       => true,
+    #   extract_path  => '/opt',
+    #   creates       => $install_path,
+    #   cleanup       => 'true',
+    #   require       => File[$install_path],
+    # }
+
     iis_site { 'basic-site':
       ensure          => 'started',
       physicalpath    => 'c:/inetpub/basic',
-      applicationpool => 'minimal_site_app_pool',
+      applicationpool => 'basic_site_app_pool',
       require         => File['basic'],
     }
 }
