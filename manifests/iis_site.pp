165cbc23fae174961bd56af871c1ed0587ad9f15
kjadhav2021 <kjadhav102015@gmail.com>
2021-07-09 04:55:41 +0800

    added different extract path and code to restart basic site

--- manifests/iis_site.pp
+++ manifests/iis_site.pp
@@ -10,18 +10,18 @@ class iis_webserver::iis_site {
       path   => 'c:/inetpub/basic',
     }
     archive { 'index.zip':
-      path         => 'C:/inetpub/basic/index.zip',
+      # path         => 'C:/inetpub/basic/index.zip',
       source       => 'https://gist.github.com/dylanratcliffe/af0e24303d241b888152bd1cd7c9063d/archive/ad273bebc01c6dac176da7a5f3c38c4d9a584521.zip',
       extract      => true,
       # extract_path => 'C:/inetpub/wwwroot/',
-      extract_path => 'C:/inetpub/basic', #directory inside tgz
-      cleanup      => false,
+      extract_path => 'C:/tmppackage', #directory inside tgz
+      cleanup      => true,
       require      => File['basic'],
     }
     file { 'index.html':
       ensure  => 'file',
       path    => 'c:/inetpub/basic/index.html',
-      source  => 'C:/inetpub/basic/af0e24303d241b888152bd1cd7c9063d-ad273bebc01c6dac176da7a5f3c38c4d9a584521/index.html',
+      source  => 'C:/tmppackage/af0e24303d241b888152bd1cd7c9063d-ad273bebc01c6dac176da7a5f3c38c4d9a584521/index.html',
       require => Archive['index.zip'],
     }
     iis_site { 'basic-site':
@@ -29,5 +29,6 @@ class iis_webserver::iis_site {
       physicalpath    => 'c:/inetpub/basic',
       applicationpool => 'basic_site_app_pool',
       require         => File['basic'],
+      subscribe       => File['index.html'],
     }
 }
