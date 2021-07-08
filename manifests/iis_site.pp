50d8c58d0a55e65c67e2713c2dddf39369ee25ee
kjadhav2021 <kjadhav102015@gmail.com>
2021-07-09 04:33:36 +0800

    updated iis_site.pp
    replaced extract_path with basic directory

--- manifests/iis_site.pp
+++ manifests/iis_site.pp
@@ -9,17 +9,15 @@ class iis_webserver::iis_site {
       ensure => 'directory',
       path   => 'c:/inetpub/basic',
     }
-
     archive { 'index.zip':
       path         => 'C:/inetpub/basic/index.zip',
       source       => 'https://gist.github.com/dylanratcliffe/af0e24303d241b888152bd1cd7c9063d/archive/ad273bebc01c6dac176da7a5f3c38c4d9a584521.zip',
       extract      => true,
-      extract_path => 'C:/inetpub/wwwroot/',
-      creates      => 'C:/inetpub/basic/', #directory inside tgz
+      # extract_path => 'C:/inetpub/wwwroot/',
+      extract_path => 'C:/inetpub/basic', #directory inside tgz
       cleanup      => false,
       require      => File['basic'],
     }
-
     iis_site { 'basic-site':
       ensure          => 'started',
       physicalpath    => 'c:/inetpub/basic',
